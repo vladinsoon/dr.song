a57d3383dfbc3a133c7a4725c9c516d81fb4e105
Vladyslav Dmytruk <vladyslav@insoongroup.com>
2021-07-22 16:30:25 +0300

    add prouct page

--- src/js/main.js
+++ src/js/main.js
@@ -13,21 +13,57 @@ const parallaxImagesNoScale = document.querySelectorAll('.parallax-no-scale');
 //Variables
 const productImgLength = document.querySelectorAll('.product_main--center img').length;
 const bulletWrap = document.querySelector('.glide__bullets');
+
 const btnIngredientsMore = document.querySelector('.btn_ingredients--more');
 const ingredientsList = document.querySelector('.product_ingredients--list');
 
+const btnReviewsMore = document.querySelector('.load_more_reviews');
+const reviewsList = document.querySelector('.reviews_box');
+
 
 if (btnIngredientsMore && ingredientsList) {
+    const listArr = ingredientsList.querySelectorAll('ul li');
+    let listHeight = 0;
+    for (let i = 0; i < 3; i++) {
+        listHeight += listArr[i].offsetHeight;
+    }
+    ingredientsList.style.maxHeight = `${listHeight}px`;
+
     btnIngredientsMore.addEventListener('click', e => {
         e.preventDefault();
         if (ingredientsList.classList.contains('active')) {
-            ingredientsList.style.maxHeight = `90px`;
+            ingredientsList.style.maxHeight = `${listHeight}px`;
             btnIngredientsMore.querySelector('span').innerHTML = 'See full ingredients'
         } else {
             ingredientsList.style.maxHeight = `${ingredientsList.querySelector('ul').offsetHeight}px`;
             btnIngredientsMore.querySelector('span').innerHTML = 'Hide ingredients'
         }
         ingredientsList.classList.toggle('active');
+    })
+}
+
+if (btnReviewsMore && reviewsList) {
+    const listArr = reviewsList.querySelectorAll('.reviews_item');
+    let listHeight = 0;
+    let fullListHeight = 0;
+    for (let i = 0; i < 2; i++) {
+        listHeight += listArr[i].offsetHeight + 50;
+    }
+    for (let p = 0; p < listArr.length; p++) {
+        fullListHeight += listArr[p].offsetHeight + 50;
+    }
+    reviewsList.style.maxHeight = `${listHeight}px`;
+
+    btnReviewsMore.addEventListener('click', e => {
+        e.preventDefault();
+        if (reviewsList.classList.contains('active')) {
+            reviewsList.style.maxHeight = `${listHeight}px`;
+            btnReviewsMore.querySelector('span').innerHTML = 'Load more'
+        } else {
+            reviewsList.style.maxHeight = `${fullListHeight}px`;
+            btnReviewsMore.querySelector('span').innerHTML = 'Hide'
+        }
+        reviewsList.classList.toggle('active');
     })
 }
 
